4c9eac0ccdf4d17d6fe24f499a4447b74a9d48bc
Rocio <rgermillaclopez@gmail.com>
2023-01-09 13:52:15 +0100

    git pages

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+const typed = new Typed('.typed', {
+    strings: ['WE', 'WORK', 'TOGETHER?'
+    ],
+
+        //stringElement: '#cadenas-texto',
+        typeSpeed: 80,
+        startDelay: 300,
+        backSpeed: 75,
+        //smartBackspace: true,
+        shuffle: false,
+        backDelay: 500,
+        loop: true,
+        loopCount: false,
+        showCursor: true,
+        cursorChar: '|',
+        contentType: 'html',
+
+
+    }
+);
+
+'use strict'
+
